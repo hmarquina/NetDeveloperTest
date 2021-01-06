44dcedc8f7ab9a92b325b8047badec22cb41ae21
hmarquina <henry.marquina@outlook.com>
2021-01-05 22:06:01 -0600

    model creation

---
+++ NetDeveloperTest/Models/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDeveloperTest.Models
+{
+    public class Job
+    {
+        [Key]
+        public int JobID { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [Display(Name ="Title")]
+        public string JobTitle { get; set; }
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Created date is required")]
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ExpiresAt { get; set; }
+    }
+}
