f4d4270d675fa166eb743bae93d3a4d78af7280d
hmarquina <henry.marquina@outlook.com>
2021-01-05 23:03:12 -0600

    Start Controller Creation

---
+++ NetDeveloperTest/Controllers/JobsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetDeveloperTest.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetDeveloperTest.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
